5667264f205861d26af0300db9356cec7f9f8f10
Ronnie Stanley <RLStanley@cpe-2606-a000-86cd-2f00-bc94-e357-8011-9921.dyn6.twc.com>
2016-12-19 20:53:14 -0500

    nothing major.

--- NVC_Fan/NVCModel.swift
+++ NVC_Fan/NVCModel.swift
@@ -10,22 +10,28 @@ import Foundation
 
 class NVC {
     private var _imageURL: String!
-    private var _videoURL: String!
+    private var _videoID: String!
     private var _videoTitle: String!
+    private var _videoDescription: String!
     
     var imageURL: String {
         return _imageURL
     }
-    var videoURL: String {
-        return _videoURL
+    var videoID: String {
+        return _videoID
     }
     var videoTitle: String {
         return _videoTitle
     }
+    var videoDescription: String {
+        return _videoDescription
+    }
     
-    init(imageURL: String, videoURL: String, videoTitle: String) {
+    init(imageURL: String, videoID: String, videoTitle: String, videoDescription: String) {
         _imageURL = imageURL
-        _videoURL = videoURL
+        _videoID = videoID
         _videoTitle = videoTitle
+        _videoDescription = videoDescription
     }
 }
+
--- NVC_Fan/VideoVC.swift
+++ NVC_Fan/VideoVC.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+
 class VideoVC: UIViewController {
 
     override func viewDidLoad() {
@@ -15,21 +17,12 @@ class VideoVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func backButtonPressed() {
+        self.dismiss(animated: true, completion: nil)
     }
+
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
